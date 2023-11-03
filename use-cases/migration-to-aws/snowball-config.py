78fa29f0c68b75ea49ecba75674c0469cc7363fe
syedmouaazfarrukh <mouaazfarrukh99@gmail.com>
2023-11-03 16:51:27 +0500

    migration-to-aws-snowballconfigs

---
+++ use-cases/migration-to-aws/snowball-config.py
@@ -0,0 +1,103 @@
+import boto3
+import time
+import logging
+
+def transfer_data_to_s3_using_snowball(s3_client, snowball_client, bucket_name, snowball_job_id):
+  """Transfers data to S3 using AWS Snowball.
+
+  Args:
+    s3_client: A Boto3 S3 client.
+    snowball_client: A Boto3 Snowball client.
+    bucket_name: The name of the S3 bucket to transfer the data to.
+    snowball_job_id: The ID of the Snowball job.
+
+  Returns:
+    None.
+  """
+
+  # Get the status of the Snowball job.
+  snowball_job = snowball_client.describe_job(JobId=snowball_job_id)
+
+  # Wait for the Snowball job to complete.
+  while snowball_job['State'] != 'Completed':
+    time.sleep(60)
+    snowball_job = snowball_client.describe_job(JobId=snowball_job_id)
+
+  # Get the Snowball export task ID.
+  snowball_export_task_id = snowball_job['JobDetails']['ExportTaskId']
+
+  # Start the Snowball export task.
+  snowball_client.start_export_task(ExportTaskId=snowball_export_task_id)
+
+  # Wait for the Snowball export task to complete.
+  while snowball_job['Details']['ExportTask']['Status'] != 'Completed':
+    time.sleep(60)
+    snowball_job = snowball_client.describe_job(JobId=snowball_job_id)
+
+  # Get the S3 manifest file.
+  s3_manifest_file_name = snowball_job['Details']['ExportTask']['ManifestS3Location']['ObjectKey']
+
+  # Copy the S3 manifest file to the S3 bucket.
+  s3_client.copy_object(Bucket=bucket_name, Key=s3_manifest_file_name, CopySource={'Bucket': snowball_job['Details']['ExportTask']['ManifestS3Location']['Bucket'], 'Key': s3_manifest_file_name})
+
+  # Upload the data files to S3.
+  with open(s3_manifest_file_name, 'r') as f:
+    for line in f:
+      file_name = line.strip()
+      s3_client.upload_file(file_name, bucket_name, file_name)
+
+  # Delete the S3 manifest file.
+  s3_client.delete_object(Bucket=bucket_name, Key=s3_manifest_file_name)
+
+  # Delete the Snowball export task.
+  snowball_client.delete_export_task(ExportTaskId=snowball_export_task_id)
+
+  logging.info('Data transferred to S3 successfully.')
+
+def transfer_data_to_s3_using_datasync(s3_client, datasync_client, bucket_name, on_premises_location, datasync_task_arn):
+  """Transfers data to S3 using AWS DataSync.
+
+  Args:
+    s3_client: A Boto3 S3 client.
+    datasync_client: A Boto3 DataSync client.
+    bucket_name: The name of the S3 bucket to transfer the data to.
+    on_premises_location: The location of the on-premises data.
+    datasync_task_arn: The ARN of the DataSync task.
+
+  Returns:
+    None.
+  """
+
+  # Wait for the DataSync task to complete.
+  while datasync_client.describe_task(TaskArn=datasync_task_arn)['Status'] != 'SUCCESS':
+    time.sleep(60)
+
+  logging.info('Data transferred to S3 successfully.')
+
+if __name__ == '__main__':
+    
+    # Create S3 and Snowball clients.
+    s3_client = boto3.client('s3')
+    bucket_name = input('Enter the name of the S3 bucket to transfer the data to: ')
+    on_premises_location = input('Enter the location of the on-premises data: ')
+    
+    snowball_client = boto3.client('snowball')
+    response = snowball_client.list_jobs()
+    snowball_job_id = response['JobList'][0]['JobId']
+    
+    if snowball_job_id is None:
+        response = snowball_client.create_job(
+            JobType='IMPORT',
+            S3Bucket='mouaazodlmigrationtestbucket',
+            SnowballCapacityPreference='T1',
+            ShippingOption='STANDARD'
+        )
+
+        snowball_job_id = response['JobId']
+
+    else:
+        # Get the Snowball job ID.
+        snowball_job_id = response['JobId']
+        
+    # Transfer the data to S3 using Snow
+    transfer_data_to_s3_using_snowball(s3_client, snowball_client, bucket_name, snowball_job_id)
