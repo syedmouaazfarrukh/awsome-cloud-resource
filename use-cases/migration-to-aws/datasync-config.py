b8ddf9ef9398ce463b12c47f773a6e81e561b311
syedmouaazfarrukh <mouaazfarrukh99@gmail.com>
2023-11-04 20:18:01 +0500

    migration-to-aws-datasyncconfigs

--- use-cases/migration-to-aws/datasync-config.py
+++ use-cases/migration-to-aws/datasync-config.py
@@ -2,57 +2,6 @@ import boto3
 import time
 import logging
 
-def transfer_data_to_s3_using_snowball(s3_client, snowball_client, bucket_name, snowball_job_id):
-  """Transfers data to S3 using AWS Snowball.
-
-  Args:
-    s3_client: A Boto3 S3 client.
-    snowball_client: A Boto3 Snowball client.
-    bucket_name: The name of the S3 bucket to transfer the data to.
-    snowball_job_id: The ID of the Snowball job.
-
-  Returns:
-    None.
-  """
-
-  # Get the status of the Snowball job.
-  snowball_job = snowball_client.describe_job(JobId=snowball_job_id)
-
-  # Wait for the Snowball job to complete.
-  while snowball_job['State'] != 'Completed':
-    time.sleep(60)
-    snowball_job = snowball_client.describe_job(JobId=snowball_job_id)
-
-  # Get the Snowball export task ID.
-  snowball_export_task_id = snowball_job['JobDetails']['ExportTaskId']
-
-  # Start the Snowball export task.
-  snowball_client.start_export_task(ExportTaskId=snowball_export_task_id)
-
-  # Wait for the Snowball export task to complete.
-  while snowball_job['Details']['ExportTask']['Status'] != 'Completed':
-    time.sleep(60)
-    snowball_job = snowball_client.describe_job(JobId=snowball_job_id)
-
-  # Get the S3 manifest file.
-  s3_manifest_file_name = snowball_job['Details']['ExportTask']['ManifestS3Location']['ObjectKey']
-
-  # Copy the S3 manifest file to the S3 bucket.
-  s3_client.copy_object(Bucket=bucket_name, Key=s3_manifest_file_name, CopySource={'Bucket': snowball_job['Details']['ExportTask']['ManifestS3Location']['Bucket'], 'Key': s3_manifest_file_name})
-
-  # Upload the data files to S3.
-  with open(s3_manifest_file_name, 'r') as f:
-    for line in f:
-      file_name = line.strip()
-      s3_client.upload_file(file_name, bucket_name, file_name)
-
-  # Delete the S3 manifest file.
-  s3_client.delete_object(Bucket=bucket_name, Key=s3_manifest_file_name)
-
-  # Delete the Snowball export task.
-  snowball_client.delete_export_task(ExportTaskId=snowball_export_task_id)
-
-  logging.info('Data transferred to S3 successfully.')
 
 def transfer_data_to_s3_using_datasync(s3_client, datasync_client, bucket_name, on_premises_location, datasync_task_arn):
   """Transfers data to S3 using AWS DataSync.
@@ -76,23 +25,37 @@ def transfer_data_to_s3_using_datasync(s3_client, datasync_client, bucket_name, 
 
 if __name__ == '__main__':
     
-    # Create S3 and Snowball clients.
+    # Create S3 and Datasync clients.
     s3_client = boto3.client('s3')
-    snowball_client = boto3.client('snowball')
-
     bucket_name = input('Enter the name of the S3 bucket to transfer the data to: ')
     on_premises_location = input('Enter the location of the on-premises data: ')
-
-    # Transfer the data to S3 using Snow
-    transfer_data_to_s3_using_snowball(s3_client, snowball_client, bucket_name, snowball_job_id)
-
-    # Create a DataSync client.
+    
     datasync_client = boto3.client('datasync')
+    response = datasync_client.list_tasks()
+    datasync_task_arn = response['TaskList'][0]['TaskArn']
 
-    # Get the bucket name.
-    bucket_name = input('Enter the name of the S3 bucket to transfer the data to: ')
+    if datasync_task_arn is None:
+        
+        # Create a DataSync task.
+        response = datasync_client.create_task(
+            SourceLocationArn='arn:aws:datasync:us-east-1:123456789012:location/loc-01234567-89ab-cdef-0123-456789abcdef',
+            DestinationLocationArn=f'arn:aws:s3:::{bucket_name}',
+            TaskType='SYNC',
+            ScheduleExpression='cron(0 0 * * ? *)',
+            Name='op-to-aws-using-datasync'
+        )
 
-    # Get the on-premises location.
-    on_premises_location = input('Enter the location of the on-premises data: ')
+        # Get the DataSync task ARN.
+        datasync_task_arn = response['TaskArn']
+
+    else:
+        
+        # Get the DataSync task ARN.
+        datasync_task_arn = response['TaskArn']    
+
 
     # Transfer the data to S3 using Snow
+    transfer_data_to_s3_using_datasync(s3_client, datasync_client, bucket_name, on_premises_location, datasync_task_arn)
+
+
+
