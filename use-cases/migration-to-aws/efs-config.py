9907ad8fc49e8c94ccd262cda8229eda3f19f500
syedmouaazfarrukh <mouaazfarrukh99@gmail.com>
2023-11-06 11:59:30 +0500

    migration-to-aws-efsconfigs

--- use-cases/migration-to-aws/efs-config.py
+++ use-cases/migration-to-aws/efs-config.py
@@ -1,7 +1,6 @@
 import boto3
 
-# Create an EFS client.
-efs_client = boto3.client('efs')
+
 
 # Create an EFS file system.
 response = efs_client.create_file_system(
@@ -26,3 +25,23 @@ ip_address = response['NetworkInterfaceId']
 
 # Mount the file system to an EC2 instance.
 # sudo mount -t nfs ${ip_address}:/ /mnt/efs
+
+
+if __name__ == "__main__":
+    
+    bucket_name = "mouaazodlmigrationtestbucket"
+    region_name = "eu-north-1" #Stockholm
+    keep_bucket = None
+    s3_resource = boto3.resource("s3")
+    
+    
+    file_path = ""
+    object_name = "sample object"
+    
+    list_buckets(s3_resource)
+    create_bucket(s3_resource, bucket_name)
+    upload_to_s3(bucket_name, file_path, object_name)
+    set_lifecycle_policy(bucket_name)
+    
+    delete_bucket(s3_resource, bucket_name)
+    list_buckets(s3_resource)
