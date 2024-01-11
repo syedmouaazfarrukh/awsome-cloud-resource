81f5eb787a6d16b1b1f45e17a8a012baeb07eef9
syedmouaazfarrukh <mouaazfarrukh99@gmail.com>
2024-01-11 17:25:55 +0500

    containers/leveraging-batch-processing-on-AWS

--- use-cases/containers/setup-your-problem/main.py
+++ use-cases/containers/setup-your-problem/main.py
@@ -4,8 +4,8 @@ import boto3
 
 #  ----------- EXTRACT -----------------
 # Load Excel files into pandas DataFrames
-file1_path = 'D://OneDrive - Octopus Digital//Desktop//HandsOnClouds//hands-on-clouds//use-cases//containers//dataset//olist_order_payments_dataset.csv'
-file2_path = 'D://OneDrive - Octopus Digital//Desktop//HandsOnClouds//hands-on-clouds//use-cases//containers//dataset//olist_order_items_dataset.csv'
+file1_path = 'D://OneDrive - Octopus Digital//Desktop//HandsOnClouds//hands-on-clouds//use-cases//containers//containerize-etl-jobs//etl-order//olist_order_payments_dataset.csv'
+file2_path = 'D://OneDrive - Octopus Digital//Desktop//HandsOnClouds//hands-on-clouds//use-cases//containers//containerize-etl-jobs//etl-order//olist_order_items_dataset.csv'
 
 df1 = pd.read_csv(file1_path)
 df2 = pd.read_csv(file2_path)
@@ -15,7 +15,7 @@ df2 = pd.read_csv(file2_path)
 #  ----------- TRANSFORM -----------------
 
 merged_df = pd.merge(df1, df2, on='order_id', how='inner')
-# print(merged_df)
+print(merged_df)
 
 # Additional transformations as needed
 
@@ -71,19 +71,19 @@ merged_df = pd.merge(df1, df2, on='order_id', how='inner')
 #  ----------- LOAD -----------------
 
 # Replace the placeholders with your actual RDS information
-username = 'admin'
-password = 'BgYlwxiBBmYEWf8LgylG'
-host = 'etlb.c3g2sa6aqpxp.eu-north-1.rds.amazonaws.com'
-port = '3306'  # Default is usually 3306 for MySQL
-database = 'ETLB_AWS'
+# username = 'your-username'
+# password = 'password'
+# host = 'endpoint'
+# port = 'port'  # Default is usually 3306 for MySQL
+# database = 'your-database-name'
 
-# Construct the connection string
-connection_string = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
+# # Construct the connection string
+# connection_string = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
 
-# Create the SQLAlchemy engine
-engine = create_engine(connection_string)
-print(engine)
+# # Create the SQLAlchemy engine
+# engine = create_engine(connection_string)
+# print(engine)
 
-table_name = 'merged_on_orderID'
-load = merged_df.to_sql(table_name, con=engine, index=False, if_exists='replace')
-print(f'Table "{table_name}" created successfully.')
+# table_name = 'merged_on_orderID'
+# load = merged_df.to_sql(table_name, con=engine, index=False, if_exists='replace')
+# print(f'Table "{table_name}" created successfully.')
